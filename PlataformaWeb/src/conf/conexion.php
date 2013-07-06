6cd279d83048d998838ee23cd9a3edfd2ea68df5
edsotecho <edso.techo@gmail.com>
2013-07-06 10:51:48 -0600

    Archivos sitio web de Catastro Nacional de Asentamientos 2013. Commit by Jimmy.

---
+++ PlataformaWeb/src/conf/conexion.php
@@ -0,0 +1,13 @@
+<?php
+	// datos para la conexion a mysql
+	define('DB_SERVER','localhost');
+	define('DB_NAME','CatastroNacional');
+	define('DB_USER','root');
+	define('DB_PASS','$Krat05$');
+	
+	//Estableciendo conexion
+	$con = mysql_connect(DB_SERVER,DB_USER,DB_PASS)
+		or die ("Error al conectar a la base de datos.");
+	mysql_select_db(DB_NAME,$con)
+		or die ("Conexion establecida, error selecionando la base de datos");
+?>
