5b849806f2ba38f82d6de47e524fb0019b4bd5da
Ching <wching1034@gmail.com>
2013-07-06 16:15:46 -0600

    Iniciando los cambios en el index después del login

--- PlataformaWeb/site/index.php
+++ PlataformaWeb/site/index.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<link href="src/css/bootstrap.min.css" rel="stylesheet" media="screen">
+ 	<script src="src/js/bootstrap.min.js"></script>
+ 	<script src="http://code.jquery.com/jquery.js"></script>
+	<?php
+	?>
+	<head>
+	</head>
+	<body>
+		<div>
+			<ul class="nav nav-tabs">
+    			<li class="active">
+    				<a href="#">Home</a>
+    			</li>
+    			<li><a href="#">...</a></li>
+    			<li><a href="#">...</a></li>
+    		</ul>
+		</div>
+	</body>
+
+<footer>
+</footer>
+</html>
