d48cfb582455e5e50f76d5977b003e1f0b1a33fd
Ching <wching1034@gmail.com>
2013-07-06 22:29:39 -0600

    Cambios

--- PlataformaWeb/src/js/funcionesVarias.js
+++ PlataformaWeb/src/js/funcionesVarias.js
@@ -10,14 +10,12 @@ function muestraDiv(valorCambio)
     if (valorCambio == 1) // Pone el div de Consultas visible
     {
       DatosDiv.style.display = "none";
-      ReportesDiv.style.display = "none";
       UsuariosDiv.style.display = "none";
       ConsultasDiv.style.display = "block";
       ConsultasLink.class = "active";
     }
     if (valorCambio == 2) // Pone el div de Datos visible
     {
-      ReportesDiv.style.display = "none";
       UsuariosDiv.style.display = "none";
       ConsultasDiv.style.display = "none"; 
       DatosDiv.style.display = "block";
@@ -26,7 +24,6 @@ function muestraDiv(valorCambio)
     if (valorCambio == 3) // Pone el div de Usuarios visible
     {
       ZonasDiv.style.display = "none";
-      ReportesDiv.style.display = "none";
       ConsultasDiv.style.display = "none";
       UsuariosDiv.style.display = "block";
       UsuariosLink.class = "active";
