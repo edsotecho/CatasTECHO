5cf9cbf935e3a1c350f7ef7237e6cd1b2ef7600f
Ching <wching1034@gmail.com>
2013-07-11 02:22:57 -0600

    cambios

--- PlataformaWeb/site/voluntario.php
+++ PlataformaWeb/site/voluntario.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
-	
 	<?php
 		include("/var/www/catasTECHO/src/cmp/encabezado.php");
 	?>
+	<head>
+		<div class="navbar">
+    		<div class="navbar-inner">
+   				<a class="brand" href="voluntario.php">CatasTECHO</a>
+    			<ul class="nav">
+    				<li id="linkConsultas" class="active"><a onClick="muestraDivVol(1)">Consultas</a></li>
+    				<li id="linkDatos"><a onClick="muestraDivVol(2)">Datos</a></li>
+    			</ul>
+    			<ul class="nav pull-right">
+    				<li class="divider-vertical"></li>
+    				
+    				<!-- referenciar a la página de los datos personales de cada usuario -->
+    				<li><a href="#">Usuario</a></li>
+    			</ul>
+    		</div>
+    	</div>
+	</head>
 	
 	
 	
@@ -11,5 +27,5 @@
 	<?php
     	include("/var/www/catasTECHO/src/cmp/estilos.php");
 	?>
-</footer>
+	</footer>
 </html>
