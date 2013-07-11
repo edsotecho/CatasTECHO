25b3b90975aeaae973e0c5d657438cdec304f874
Ching <wching1034@gmail.com>
2013-07-10 20:56:12 -0600

    cambios

--- PlataformaWeb/site/voluntario.php
+++ PlataformaWeb/site/voluntario.php
@@ -1,1 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+	
+	<?php
+		include("/var/www/catasTECHO/src/cmp/encabezado.php");
+	?>
+	
+	
+	
+	<footer>
+	<?php
+    	include("/var/www/catasTECHO/src/cmp/estilos.php");
+	?>
+</footer>
+</html>
