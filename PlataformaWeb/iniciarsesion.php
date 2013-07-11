ac714ab1c283114010ea051de458bee9cfe9ee4d
Ching <wching1034@gmail.com>
2013-07-11 06:29:50 -0600

    cambios

---
+++ PlataformaWeb/iniciarsesion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<?php
+		include("/var/www/catasTECHO/src/cmp/encabezado.php");
+	?>
+	
+	
+	
+	<footer>
+	<?php
+    	include("/var/www/catasTECHO/src/cmp/estilos.php");
+	?>
+	</footer>
+</html>
